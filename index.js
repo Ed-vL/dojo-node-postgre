ecdb73bf8b83d82a702f40c030ec6dc2cdb58742
Eduardo Vieira <eduardolima.df@gmail.com>
2021-09-02 12:04:52 -0300

    Cleaning up comments

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-const bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes')
 
 var app = express()
-//app.use(cors())
-
-//app.get('/', function (req, res) {
-//  res.send('hello world')
-//})
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/routes', router)
-//require('./routes')(app);
-//router(app)
 
 app.listen(3000)
